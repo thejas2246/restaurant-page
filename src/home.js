be9309f1cfd059b85134e99d35aa9ce515eef263
Thejas A <thejas2246@gmail.com>
2025-06-27 10:56:26 +0530

    create a function to generate home page content

--- src/home.js
+++ src/home.js
@@ -1,8 +1,30 @@
-import HomeFoodImage from "./assets/images/food.jpg";
-
 export function showHome() {
   const mainContainer = document.querySelector("#content");
-  const foodImageElement = document.createElement("img");
-  foodImageElement.src = HomeFoodImage;
-  mainContainer.appendChild(foodImageElement);
+  generateContent(mainContainer);
+}
+
+function generateContent(mainContainer) {
+  const subContainer = document.createElement("div");
+  subContainer.setAttribute("class", "sub-container");
+
+  const homePageHeading = document.createElement("h1");
+  homePageHeading.setAttribute("class", "home-page-heading hi");
+
+  const homePagePara1 = document.createElement("p");
+  homePagePara1.setAttribute("class", "home-page-para-1");
+
+  const homePagePara2 = document.createElement("p");
+  homePagePara2.setAttribute("class", "home-page-para-2");
+
+  homePageHeading.textContent = "Welcome to TasteHub Restaurant";
+  homePagePara1.textContent =
+    "Discover a world of exquisite flavors and memorable dining.";
+  homePagePara2.textContent =
+    "Join us for an unforgettable culinary journey where every dish is crafted with passion and precision. We combine fresh, local ingredients with innovative techniques to bring you an unparalleled dining experience.";
+
+  subContainer.appendChild(homePageHeading);
+  subContainer.appendChild(homePagePara1);
+  subContainer.appendChild(homePagePara2);
+
+  mainContainer.appendChild(subContainer);
 }
